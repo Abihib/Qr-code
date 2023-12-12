82f5877108338de68e89716696469028b9b9b197
gun <50666147+Abihib@users.noreply.github.com>
2023-12-12 17:34:32 +0300

    1

--- g_qr.py
+++ g_qr.py
@@ -6,13 +6,13 @@ from qrcode.image.styles.colormasks import (SolidFillColorMask)
 
 for i in range(3):
     qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-    qr.add_data('https://vmeste30.santagroup.by/rassadka/' + str(i))
+    qr.add_data('https:///rassadka/' + str(i))
     qr = qr.make_image(image_factory=StyledPilImage,
                      color_mask=SolidFillColorMask((255,255,255),(0,47,112)),
                      module_drawer=RoundedModuleDrawer(),
                      #embeded_image_path="pngegg.png",
                      eye_drawer=RoundedModuleDrawer())
-    qr.save('vmeste30_' + str(i).rjust(0, '0') + '.png')
+    qr.save('vmeste_' + str(i).rjust(0, '0') + '.png')
 
 
 
