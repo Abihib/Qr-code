a6300c2ed9bb4de8c98fe646782415d4e41f0a3c
gun <50666147+Abihib@users.noreply.github.com>
2023-12-12 17:17:05 +0300

    Add files via upload

---
+++ g_qr.py
@@ -0,0 +1,15 @@
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers.pil import (RoundedModuleDrawer )
+from qrcode.image.styles.colormasks import (SolidFillColorMask)
+
+
+for i in range(3):
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+    qr.add_data('https://vmeste30.santagroup.by/rassadka/' + str(i))
+    qr = qr.make_image(image_factory=StyledPilImage,
+                     color_mask=SolidFillColorMask((255,255,255),(0,47,112)),
+                     module_drawer=RoundedModuleDrawer(),
+                     #embeded_image_path="pngegg.png",
+                     eye_drawer=RoundedModuleDrawer())
+    qr.save('vmeste30_' + str(i).rjust(0, '0') + '.png')
